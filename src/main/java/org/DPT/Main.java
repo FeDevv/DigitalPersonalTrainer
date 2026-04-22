f137b38d952ae848b9b0cf74aab83bce4c99404c
FeDevv <ricobon03@gmail.com>
2026-04-22 11:30:38 +0200

    directions

--- src/main/java/org/DPT/Main.java
+++ src/main/java/org/DPT/Main.java
@@ -33,3 +33,13 @@ public class Main {
         }
     }
 }
+
+/*
+1. Lasciare il main pulito e creare un file appController (che sarà l'orchestrator del progetto, dato che lavoriamo per pacchetti)
+2. attualmente il flusso di operazioni per il login è main -> LoginCLIController -> LoginController. Vorrei far si che il punto d'ingresso sia sempre
+il controller logico, che in base al tipo di scelta di interfaccia, istanzia e avvia il corretto controller grafico.
+3. dato che voglio una struttura "pronta ad essere espansa", direi di creare il modulo BOOT (o semplicemente implemetarlo in appController) che
+manterrà tutta la logica di avvio ("scegli il tipo di interfaccia", boot dei servizi ...)
+4. usiamo 1 scanner solo, lo creiamo all'inizio e facciamo dependency injection nei pacchetti che lo necessitano, ciò risulta facile in quanto abbiamo
+l'elemento centrale "appController" che fa da collante tra i vari package.
+*/
