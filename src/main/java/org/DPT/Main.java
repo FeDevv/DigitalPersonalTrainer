387b9bc1ba80c1e69076944b1ff6bcee1e6ad5b6
FeDevv <ricobon03@gmail.com>
2026-04-22 14:46:15 +0200

    alignment

--- src/main/java/org/DPT/Main.java
+++ src/main/java/org/DPT/Main.java
@@ -13,15 +13,5 @@ public class Main {
         Orchestrator orchestrator = new Orchestrator();
         orchestrator.run(args);
     }
+
 }
-
-/*
-1. attualmente il flusso di operazioni per il login è main -> LoginCLIController -> LoginController. Vorrei far si che il punto d'ingresso sia sempre
-il controller logico, che in base al tipo di scelta di interfaccia, istanzia e avvia il corretto controller grafico.
-
-2. dato che voglio una struttura "pronta ad essere espansa", direi di creare il modulo BOOT (o semplicemente implemetarlo in appController) che
-manterrà tutta la logica di avvio ("scegli il tipo di interfaccia", boot dei servizi ...)
-
-3. usiamo 1 scanner solo, lo creiamo all'inizio e facciamo dependency injection nei pacchetti che lo necessitano, ciò risulta facile in quanto abbiamo
-l'elemento centrale "appController" che fa da collante tra i vari package.
-*/
