c17d017f0272018fc5d4ee4f00a0997703eb5252
FeDevv <ricobon03@gmail.com>
2026-05-08 21:19:11 +0200

    SonarQube

--- src/main/java/org/DPT/Main.java
+++ src/main/java/org/DPT/Main.java
@@ -14,4 +14,4 @@ public class Main {
 
 // check CODICE IN INGLESE, OUTPUT IN ITALIANO -> in PROSSIMI PASSI.txt le parti da rivedere
 
-// To resume this session: gemini --resume 564786ed-b1b2-4a47-a7a7-e603eda96da5 
+// To resume this session: gemini --resume 564786ed-b1b2-4a47-a7a7-e603eda96da5
