4cb38080ddcbf90b64f6f140a16be1cbbdbf4646
FeDevv <ricobon03@gmail.com>
2026-05-16 18:50:21 +0200

    solved code smell in tests.

--- src/test/java/org/dpt/user/management/dto/ClientCreationDTOTest.java
+++ src/test/java/org/dpt/user/management/dto/ClientCreationDTOTest.java
@@ -19,49 +19,54 @@ class ClientCreationDTOTest {
     @Test
     @DisplayName("Creazione valida: cliente maggiorenne con CF corretto")
     void constructor_ValidData_Success() {
+        LocalDate birthDate = LocalDate.now().minusYears(20);
         assertDoesNotThrow(() -> new ClientCreationDTO(
             validUserBase,
             "RSSMRA80A01H501Z", // 16 caratteri
             "Via Roma 1",
-            LocalDate.now().minusYears(20)
+            birthDate
         ));
     }
 
     @Test
     @DisplayName("Codice Fiscale errato: dovrebbe lanciare ValidationException se != 16 caratteri")
     void constructor_InvalidFiscalCode_ThrowsException() {
+        LocalDate birthDate = LocalDate.now().minusYears(20);
+        
         // Troppo corto
         assertThrows(ValidationException.class, () -> new ClientCreationDTO(
-            validUserBase, "SHORT123", "Via Roma 1", LocalDate.now().minusYears(20)
+            validUserBase, "SHORT123", "Via Roma 1", birthDate
         ));
         
         // Troppo lungo
         assertThrows(ValidationException.class, () -> new ClientCreationDTO(
-            validUserBase, "RSSMRA80A01H501Z_EXTRA", "Via Roma 1", LocalDate.now().minusYears(20)
+            validUserBase, "RSSMRA80A01H501Z_EXTRA", "Via Roma 1", birthDate
         ));
     }
 
     @Test
     @DisplayName("Limite età: dovrebbe lanciare ValidationException per utenti sotto i 14 anni")
     void constructor_TooYoung_ThrowsException() {
-        LocalDate today = LocalDate.now();
+        LocalDate tooYoungDate = LocalDate.now().minusYears(13);
+        LocalDate exactAgeDate = LocalDate.now().minusYears(14);
         
         // 13 anni (non ammesso)
         assertThrows(ValidationException.class, () -> new ClientCreationDTO(
-            validUserBase, "RSSMRA80A01H501Z", "Via Roma 1", today.minusYears(13)
+            validUserBase, "RSSMRA80A01H501Z", "Via Roma 1", tooYoungDate
         ));
 
         // Esattamente 14 anni (ammesso)
         assertDoesNotThrow(() -> new ClientCreationDTO(
-            validUserBase, "RSSMRA80A01H501Z", "Via Roma 1", today.minusYears(14)
+            validUserBase, "RSSMRA80A01H501Z", "Via Roma 1", exactAgeDate
         ));
     }
 
     @Test
     @DisplayName("UserBase mancante: dovrebbe lanciare ValidationException")
     void constructor_NullUserBase_ThrowsException() {
+        LocalDate birthDate = LocalDate.now().minusYears(20);
         assertThrows(ValidationException.class, () -> new ClientCreationDTO(
-            null, "RSSMRA80A01H501Z", "Via Roma 1", LocalDate.now().minusYears(20)
+            null, "RSSMRA80A01H501Z", "Via Roma 1", birthDate
         ));
     }
 }
